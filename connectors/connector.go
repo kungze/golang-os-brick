e50722c37adb9c6919148b9b2bec9c39314283f2
fightdou <1426589337@qq.com>
2022-01-05 10:30:38 +0800

    实现 rbd connector

---
+++ connectors/connector.go
@@ -0,0 +1,24 @@
+package connectors
+
+import (
+	"github.com/kungze/golang-os-brick/connectors/rbd"
+	"strings"
+)
+
+// ConnProperties is base class interface
+type ConnProperties interface {
+	ConnectVolume() (map[string]string, error)
+	DisConnectVolume()
+	ExtendVolume() (int64, error)
+}
+
+// NewConnector Build a Connector object based upon protocol and architecture
+func NewConnector(protocol string, connInfo map[string]interface{}) ConnProperties {
+	switch strings.ToUpper(protocol) {
+	case "RBD":
+		// Only supported local attach volume
+		connInfo["do_local_attach"] = true
+		return rbd.NewRBDConnector(connInfo)
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kungze/golang-os-brick
+
+go 1.17
+
+require github.com/wonderivan/logger v1.0.0
